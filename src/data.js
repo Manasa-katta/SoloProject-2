f89169378ac54d92c98882d4a11b4102154a3667
Manasa-katta <kattamanasa1911@gmail.com>
2024-10-05 13:08:35 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,84 @@
+
+const data = [
+    {
+        id: 1,
+        title: "Life Lessons with Katie Zaferes",
+        description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+        price: 136,
+        coverImg: "Katie-zaferes.jpeg",
+        stats: {
+            rating: 5.0,
+            reviewCount: 6
+        },
+        location: "Online",
+        openSpots: 0,
+    },
+    {
+        id: 2,
+        title: "Learn Wedding Photography",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 125,
+        coverImg: "wedding-photography.jpg",
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: "Online",
+        openSpots: 27,
+    },
+    {
+        id: 3,
+        title: "Group Mountain Biking",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "mountain-bike.jpg",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "Norway",
+        openSpots: 3,
+    },
+    {
+        id: 4,
+        title: "Guitar for Beginners",
+        description: "This course is perfect for aspiring guitarists who want to learn how to play. From learning basic chords to understanding music theory, you’ll be able to play your favorite songs in no time.",
+        price: 100,
+        coverImg: "guitar-beginners.jpg",
+        stats: {
+            rating: 4.9,
+            reviewCount: 20
+        },
+        location: "In-Person",
+        openSpots: 15,
+    },
+    {
+        id: 5,
+        title: "Yoga and Mindfulness",
+        description: "Discover the benefits of yoga and mindfulness with this beginner-friendly course. Learn techniques to reduce stress, improve flexibility, and enhance your well-being.",
+        price: 80,
+        coverImg: "yoga-mindfulness.jpg",
+        stats: {
+            rating: 5.0,
+            reviewCount: 35
+        },
+        location: "Online",
+        openSpots: 5,
+    },
+    {
+        id: 6,
+        title: "Mastering Italian Cooking",
+        description: "Dive into the rich flavors of Italy with this hands-on cooking course. Learn to make classic dishes like pasta, risotto, and tiramisu. Perfect for beginners and intermediate cooks who want to impress their family and friends.",
+        price: 90,
+        coverImg: "italian-cooking.jpg",
+        stats: {
+            rating: 4.9,
+            reviewCount: 40
+        },
+        location: "In-Person",
+        openSpots: 8
+    }
+    
+]
+
+export default data
